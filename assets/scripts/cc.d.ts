6dfd3baa0a8554e1e1103f87915034a02dfc72af
ssp <ssp007a@gmail.com>
2026-02-26 02:33:01 +0800

    feat: update tsconfig.json to include all UI scripts (#2)
    - Change include from hardcoded file list to wildcard pattern
    - Add proper type declarations in cc.d.ts for Cocos Creator types

--- assets/scripts/cc.d.ts
+++ assets/scripts/cc.d.ts
@@ -15,7 +15,44 @@ export type Sprite = any;
 export type SpriteFrame = any;
 export type EventTouch = any;
 
-// Decorator functions
-export function _decorator(target: any): any;
-export const property: any;
+// Decorator functions - _decorator is an object with ccclass and property
+export const _decorator: {
+    ccclass: any;
+    property: any;
+};
 export const ccclass: any;
+export const property: any;
+
+// Provide mock constructors for value usage (new Color(), etc.)
+export const Component: new (...args: any[]) => any;
+export const Node: new (...args: any[]) => any;
+export const Color: new (...args: any[]) => any;
+export const UITransform: new (...args: any[]) => any;
+export const Graphics: new (...args: any[]) => any;
+
+// Label with static enums
+export interface LabelConstructor {
+    new (...args: any[]): any;
+    HorizontalAlign: { CENTER: any; LEFT: any; RIGHT: any };
+    VerticalAlign: { CENTER: any; TOP: any; BOTTOM: any };
+}
+export const Label: LabelConstructor;
+
+export const Sprite: new (...args: any[]) => any;
+export const SpriteFrame: new (...args: any[]) => any;
+
+// EventTouch with static properties
+export interface EventTouchConstructor {
+    new (...args: any[]): any;
+    TOUCH_END: any;
+    TOUCH_START: any;
+    TOUCH_MOVE: any;
+}
+export const EventTouch: EventTouchConstructor;
+
+export const SpriteAtlas: new (...args: any[]) => any;
+
+// Enum values
+export const HorizontalAlign: any;
+export const VerticalAlign: any;
+export const TouchEvent: { TOUCH_END: any; TOUCH_START: any; TOUCH_MOVE: any };
